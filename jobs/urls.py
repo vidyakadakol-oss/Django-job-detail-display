9616497d377e28c40153e410316ed3164b5d0007
vidyakadakol-oss <vidyakadakol4887@gmail.com>
2026-03-05 21:21:34 +0530

    Initial commit - Django Job Search Project

---
+++ jobs/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.job_list, name='job_list'),
+    path('job/<int:job_id>/', views.job_detail, name='job_detail'),
+]
